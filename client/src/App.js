cb05f7d88e7358483f3b1fdf5a69c06a392d553c
yokevi <you.k2208@gmail.com>
2021-04-21 14:55:33 -0700

    added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
+import NavBar from './components/NavBar'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Roboto'
+  }
+})
 
 const App = props => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <ThemeProvider theme={theme} >
+      <div>
+        <NavBar />
+        Hello World!
+      </div>
+    </ThemeProvider>
   )
 }
 
